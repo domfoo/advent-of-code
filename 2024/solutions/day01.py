50670fc550b3eb3b90c0a1dc876e358a10ba39ed
Dominik Bartsch <dominik.bartsch@gmx.net>
2024-12-01 16:53:21 +0100

    Add solutions for day 1

--- 2024/solutions/day01.py
+++ 2024/solutions/day01.py
@@ -1,5 +1,19 @@
-def part1(data: str):
-    pass
+# day01.py
 
-def part2(data: str):
-    pass
+from collections import Counter
+
+
+def get_sorted_lists_from_data(data: str) -> (list[int], list[int]):
+    numbers = [int(x) for x in data.split()]
+    return (sorted(numbers[::2]), sorted(numbers[1::2]))
+
+
+def part1(data: str) -> int:
+    left_list, right_list = get_sorted_lists_from_data(data)
+    return sum([abs(b - a) for a, b in zip(left_list, right_list)])
+
+
+def part2(data: str) -> int:
+    left_list, right_list = get_sorted_lists_from_data(data)
+    counter = Counter(right_list)
+    return sum([number * counter.get(number, 0) for number in left_list])
